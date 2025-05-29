abfee14077c957cd2d12fede0494b92b91620698
Konfus-dev <63468146+Konfus-dev@users.noreply.github.com>
2025-05-28 17:08:48 -0700

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -3,39 +3,25 @@ project "Diligent Renderer"
     language "C++"
     cppdialect "C++20"
     staticruntime "Off"
-    ignoredefaultlibraries { "MSVCRT" }
-    externalwarnings "Off"
 
-    targetdir ("../../" .. OutputTargetPluginDir .. "")
-    objdir    ("../../" .. OutputIntermediatePluginDir .. "")
+    RegisterDynamicPlugin("Diligent Renderer")
 
     files
     {
-        "./Source/**.h",
-        "./Source/**.c",
-        "./Source/**.cc",
-        "./Source/**.hpp",
-        "./Source/**.cpp",
-        "./Include/**.h",
-        "./Include/**.c",
-        "./Include/**.cc",
-        "./Include/**.hpp",
-        "./Include/**.cpp",
-        "./**.plugin",
+        "./**.hpp",
+        "./**.cpp",
+        "./**.h",
+        "./**.c",
         "./**.md",
-        "./*.lua"
+        "./**.lua",
+        "./**.txt",
+        "./**.plugin"
     }
-
     includedirs
     {
         "./Source",
-        "%{Using.DiligentEngine}"
-    }
 
+    }
     links
     {
-        "DiligentFX"
     }
-
-    ToyboxPluginConfigs()
-    RegisterPlugin("Diligent Renderer")
